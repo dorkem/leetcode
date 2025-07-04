a6a26b0b9ab67673db242c4f6f10d4657aa3a8d9
최재혁 <20233144@gs.cwnu.ac.kr>
2025-07-04 15:25:31 +0900

    Time: 397 ms (5.34%), Space: 67.7 MB (6.24%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    self_views = views[views['author_id'] == views['viewer_id']]
+    result = self_views[['author_id']].drop_duplicates().sort_values(by='author_id')
+    return result.rename(columns={'author_id': 'id'})
